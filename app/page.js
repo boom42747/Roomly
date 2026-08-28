e1902ce74df3aeef819f0bcfd4d709b58173f1cb
boom42747 <boom42747@gmail.com>
2026-08-28 16:47:18 -0400

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Roomly</h1>
+      <p>ค้นหาบ้านและคอนโดที่เหมาะกับคุณ</p>
+
+      <input
+        type="text"
+        placeholder="ค้นหาคอนโด ทำเล BTS/MRT..."
+      />
+
+      <button>ค้นหา</button>
+    </main>
+  );
+}
